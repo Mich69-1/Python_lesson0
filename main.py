35cc52996f3ecdd4b6958db02250abe00b0256f7
Mich69-1 <berlandme@gmail.com>
2024-08-13 09:02:17 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# 4th program
+a = '2123.856'
+print(int(float(a) * 10 % 10))
+
+# 4th program, old version
+a = 13.42
+b = 42.13
+print(int(a) == int(round(b % 1 * 100, 2)) or int(b) == int(round(a % 1 * 100, 2)))
+
