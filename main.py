ac9662bb374944b457e317ea5d3ac387dc245823
Mich69-1 <berlandme@gmail.com>
2024-08-13 20:54:30 +0500

    complete exercise

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+x = 2 * 2 + 2
+y = 2 * (2 + 2)
+print(x)
+print(y)
+print(x == y)
+
 # 4th program
 a = '2123.856'
 print(int(float(a) * 10 % 10))
 
-# 4th program, old version
+# 4th program, old (and better) version
 a = 13.42
 b = 42.13
 print(int(a) == int(round(b % 1 * 100, 2)) or int(b) == int(round(a % 1 * 100, 2)))
